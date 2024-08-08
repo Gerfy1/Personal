5eb3354bf8fa69869dc89d7e83faf18e475a847c
Gerfy <110788311+Gerfy1@users.noreply.github.com>
2024-08-08 19:15:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        document.querySelector(".loading-screen").style.display = "none";
+        document.querySelector(".content").style.display = "block";
+    }, 7000); 
+});
